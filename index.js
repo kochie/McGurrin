5ba152a5a561f2c8b04bde7c7b93654c9f0c8161
Robert Koch <robert@kochie.io>
2018-04-07 15:04:57 +1000

    Setup Electron and React.
    Basic electron app runs and React gets bundled with webpack into a file.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const {app, BrowserWindow} = require('electron')
-  const path = require('path')
-  const url = require('url')
-  
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }))
-  }
-  
-  app.on('ready', createWindow)
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+}
+
+app.on('ready', createWindow);
+
---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const app = () => (
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+export default app;
